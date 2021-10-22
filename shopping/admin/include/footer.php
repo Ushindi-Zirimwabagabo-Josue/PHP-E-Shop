45b91df2af5b012dbec57676f3bf75004598561f
Josue Zirimwabagabo <josue.zirimwabagabo@sierralines.com>
2021-10-22 17:00:08 +0200

    first commit

---
+++ shopping/admin/include/footer.php
@@ -0,0 +1,7 @@
+	<div class="footer">
+		<div class="container">
+			 
+
+			<b class="copyright">&copy; CRIS-Shop </b>Copyright@2021 by aminichristian email: aminichristian3@gmail.com tel: +243978162462 Tous Droits Reserves. 
+		</div>
+	</div>
---
+++ shopping/includes/myaccount-sidebar.php
@@ -0,0 +1,20 @@
+	<div class="col-md-4">
+					<!-- checkout-progress-sidebar -->
+<div class="checkout-progress-sidebar ">
+	<div class="panel-group">
+		<div class="panel panel-default">
+			<div class="panel-heading">
+		    	<h4 class="unicase-checkout-title">PROGRESSION DE VOTRE COMMANDE</h4>
+		    </div>
+		    <div class="panel-body">
+				<ul class="nav nav-checkout-progress list-unstyled">
+					<li><a href="my-account.php">Mon Compte</a></li>
+					<li><a href="bill-ship-addresses.php">Adresse de livraison/facturation</a></li>
+					<li><a href="order-history.php">Historique des Commandes</a></li>
+					<li><a href="pending-orders.php">Commande en Attente</a></li>
+				</ul>		
+			</div>
+		</div>
+	</div>
+</div> 
+<!-- checkout-progress-sidebar -->				</div>
